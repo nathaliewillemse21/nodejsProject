8b3df94f0ca6f84d8d0bcd6b199b0d149735aa8c
nathaliewillemse21 <njpwillemse69@gmail.com>
2024-02-23 15:47:24 +0200

    config

---
+++ config/index.js
@@ -0,0 +1,13 @@
+import { createPool } from 'mysql';
+import { config } from 'dotenv';
+
+config();
+let connection = createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_Name,
+  user: process.env.DB_UserName,
+  password: process.env.DB_UserPass,
+  multipleStatements: true,
+  connectionLimit: 25,
+});
+export { connection };
