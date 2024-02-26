c431d3cfeb2f12cc188feb19cb6b3f017b69589b
nathaliewillemse21 <njpwillemse69@gmail.com>
2024-02-26 09:36:19 +0200

    static

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log('Annyeonghaseyo')
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import cors from 'cors';
+import express from 'express';
+import { config } from 'dotenv';
+config();
+
+const app = express();
+const port = +process.env.PORT || 4000;
+app.use(
+  express.static('./static'),
+  express.json(),
+  express.urlencoded({
+    extended: true,
+  }),
+  cookieParser(),
+  cors()
+);
+app.get('^/$|/nodeProject', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, './static/index.html'));
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
